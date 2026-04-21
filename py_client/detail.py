5aab15981edb579cede4a5b3d064474ff13b2aa7
marufiqbal5g <kmiqbal1989@gmail.com>
2026-04-21 11:38:26 +0600

    DRF Generics RetriveAPIView

---
+++ py_client/detail.py
@@ -0,0 +1,9 @@
+import requests
+
+endpoint = 'http://localhost:8000/api/products/1/'
+
+get_response = requests.get(endpoint)
+
+print(f'text only:{get_response.text}')
+print(f'status code: {get_response.status_code}')
+print(f'json: {get_response.json()}')
